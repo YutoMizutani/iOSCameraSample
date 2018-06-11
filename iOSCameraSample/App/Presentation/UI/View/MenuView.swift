9efa31b40b3fb9205353628b4a237381a19e5cab
YutoMizutani <yuto.mizutani.dev@gmail.com>
2018-06-11 20:57:06 +0900

    :sparkles: Add launch button

--- iOSCameraSample/App/Presentation/UI/View/MenuView.swift
+++ iOSCameraSample/App/Presentation/UI/View/MenuView.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class MenuView: UIView {
+    var launchCameraButton: UIButton!
+
     required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)!
     }
@@ -19,7 +21,8 @@ class MenuView: UIView {
         configureView()
         layoutView()
     }
-    
+
+    /// 画面が更新された際に呼ばれる。この変更はこのクラスからは呼ばず，利用しているViewControllerから呼ぶようにする。
     override func layoutSubviews() {
         super.layoutSubviews()
 
@@ -30,10 +33,29 @@ class MenuView: UIView {
 }
 
 extension MenuView {
+    /// Viewを設定する。
     private func configureView() {
-
+        launchCameraButton: do {
+            self.launchCameraButton = { () -> UIButton in
+                let button = UIButton()
+                // タイトル
+                button.setTitle("Launch camera", for: .normal)
+                // 文字色
+                button.setTitleColor(UIColor(red: 0, green: 0, blue: 1, alpha: 1), for: .normal)
+                button.setTitleColor(UIColor(red: 0, green: 0, blue: 1, alpha: 0.2), for: .highlighted)
+                // フォント
+                button.titleLabel?.font = UIFont.systemFont(ofSize: 30)
+                return button
+            }()
+            self.addSubview(self.launchCameraButton)
+        }
     }
-    private func layoutView() {
 
+    /// 画面が更新された際に呼ばれる。
+    private func layoutView() {
+        launchCameraButton: do {
+            self.launchCameraButton.frame = CGRect(x: 0, y: 0, width: 300, height: 100)
+            self.launchCameraButton.center = self.center
+        }
     }
 }
