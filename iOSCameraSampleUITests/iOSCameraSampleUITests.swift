14a1a450682c891d081e609f5e55580b1a750cfe
YutoMizutani <yuto.mizutani.dev@gmail.com>
2018-06-11 20:54:13 +0900

    :white_check_mark: Add NavigationItem title test

--- iOSCameraSampleUITests/iOSCameraSampleUITests.swift
+++ iOSCameraSampleUITests/iOSCameraSampleUITests.swift
@@ -9,6 +9,7 @@
 import XCTest
 
 class iOSCameraSampleUITests: XCTestCase {
+    var app: XCUIApplication!
         
     override func setUp() {
         super.setUp()
@@ -18,7 +19,8 @@ class iOSCameraSampleUITests: XCTestCase {
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
+        self.app = XCUIApplication()
+        self.app.launch()
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
@@ -32,5 +34,9 @@ class iOSCameraSampleUITests: XCTestCase {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-    
+
+    func testMenuTitle() {
+        // MenuViewControllerのNavigationItemのTitleを判定する。
+        XCTAssert(self.app.navigationBars["iOSCameraSample"].exists)
+    }
 }
