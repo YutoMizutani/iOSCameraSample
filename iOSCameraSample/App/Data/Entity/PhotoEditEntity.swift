c5a4eec08b7334631f34d52d5b8ea83d063e9b0e
YutoMizutani <yuto.mizutani.dev@gmail.com>
2018-06-13 07:00:01 +0900

    :sparkles: Add save flag entity

--- iOSCameraSample/App/Data/Entity/PhotoEditEntity.swift
+++ iOSCameraSample/App/Data/Entity/PhotoEditEntity.swift
@@ -9,7 +9,13 @@
 import Foundation
 
 protocol PhotoEditEntity {
+    var didSaveFlag: Bool { get }
+    var didEditFlag: Bool { get }
 }
 
 struct PhotoEditEntityImpl: PhotoEditEntity {
+    /// 保存フラグ。これまで保存されたかの状態を保持する。
+    let didSaveFlag: Bool
+    /// 編集フラグ。前回保存した状態からの変化を保持する。
+    let didEditFlag: Bool
 }
