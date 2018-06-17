eaf522c402bc2e44bd9545d1fa8d4025fba3e6be
YutoMizutani <yuto.mizutani.dev@gmail.com>
2018-06-17 10:45:59 +0900

    :memo: Add comment in Repository

--- iOSCameraSample/App/Data/Repository/PhotoEditRepository.swift
+++ iOSCameraSample/App/Data/Repository/PhotoEditRepository.swift
@@ -27,17 +27,22 @@ struct PhotoEditRepositoryImpl {
 }
 
 extension PhotoEditRepositoryImpl: PhotoEditRepository {
+    /// 編集状態を取得する。
     func getState() -> PhotoEditSaveStateEntity {
-        // ここで生じるエラーは保存されていない場合のみ。try?でnilを許容する。
+        // ここで生じるエラーは保存されていない場合のみ。Entityでnilを補完するため，try?でnilを許容する。
         let didSaveFlag = try? self.dataStore.get(key: .didSaveFlag)
         let didEditFlag = try? self.dataStore.get(key: .didEditFlag)
 
         // Entityを生成して返す。
         return PhotoEditSaveStateEntityImpl(didSaveFlag: didSaveFlag, didEditFlag: didEditFlag)
     }
+
+    /// 保存状態を更新する。
     func setDidSaveState(_ state: Bool) {
         self.dataStore.push(state, key: .didSaveFlag)
     }
+    
+    /// 編集状態を更新する。
     func setDidEditState(_ state: Bool) {
         self.dataStore.push(state, key: .didEditFlag)
     }
