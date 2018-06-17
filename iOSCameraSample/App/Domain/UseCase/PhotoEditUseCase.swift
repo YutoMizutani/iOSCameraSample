c8f27eca41e27de1766a667df6d9206e520fbf25
YutoMizutani <yuto.mizutani.dev@gmail.com>
2018-06-17 10:56:31 +0900

    :memo: Add comment in UseCase

--- iOSCameraSample/App/Domain/UseCase/PhotoEditUseCase.swift
+++ iOSCameraSample/App/Domain/UseCase/PhotoEditUseCase.swift
@@ -34,20 +34,28 @@ struct PhotoEditUseCaseImpl {
 }
 
 extension PhotoEditUseCaseImpl: PhotoEditUseCase {
+    /// 保存状態のModelを取得する。
     func getSaveStateModel() -> PhotoEditSaveStateModel {
         let entity = self.repository.getState()
         return self.translator.translate(from: entity)
     }
+
+    /// UIImageView用のViewModelを取得する。
     func getImageModel(_ image: UIImage) -> PhotoEditImageModel {
         return PhotoEditImageModelImpl(image: image)
     }
+
+    /// 保存状態の変更を適用する。
     func changeSaveState(_ state: Bool) {
         self.repository.setDidSaveState(state)
     }
+
+    /// 編集状態の変更を適用する。
     func changeEditState(_ state: Bool) {
         self.repository.setDidEditState(state)
     }
 
+    /// コントラストを適用する。
     func contrast(_ image: UIImage, value: Float) -> UIImage? {
         guard let cgImage = image.cgImage else { return nil }
         let ciImage = CIImage(cgImage: cgImage)
